71e501e7e083b55fd9e32cea36a008002c8866ef
champlongC <cchamplong@mini-design.fr>
2013-12-06 10:55:02 +0100

    création

---
+++ js/index.js
@@ -0,0 +1,29 @@
+(function($){
+
+        /* déclenché quand le dom est pret 
+            (attention les fichiers externes (images, vidéo ...) ne sont pas forcément chargés 
+        */
+        $(document).ready(function (){
+        
+                // votre code travaillant sur le DOM
+        
+        });
+        
+        
+        /* optionnel
+        // déclenché quand le document a fini de tout télécharger (images ...)
+        $(window).load(function() {
+        
+                // votre code travaillant sur le DOM et les docs externes
+        
+        });
+        // déclenché quand le document est redimentionné
+        $(window).resize(function() {
+        
+                // votre code travaillant quand le navigateur est redimentionné
+        
+        });
+        
+        */
+
+})(window.jQuery);
